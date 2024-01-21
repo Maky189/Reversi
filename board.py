14326136221a9ffd5307721ff19ac222eba8ebda
Maky188 Kali Linux <maky188pgt555@gmail.com>
2024-01-21 22:20:53 +0000

    Can't get rid of the bug. I NEEEEED HEEEEELP

--- board.py
+++ board.py
@@ -5,11 +5,15 @@ from assets import variables
 from pieces import Piece
 
 #Create the grid
+file = open('./assets/user.txt', 'a')
+
+
 class Grid:
     def __init__(self):
         #define the dimension of the square
         self.size = variables.SIZE[0] / 8
         self.store_pieces = []
+
         #loops over horizontaly in 12
     
     def build(self, surface, mouse_pos, piece):
@@ -17,41 +21,13 @@ class Grid:
             pygame.draw.line(surface, variables.WHITE, start_pos=(0, i * self.size), end_pos=(variables.SIZE[0], i * self.size), width=5)
             for j in range(12):
                 pygame.draw.line(surface, variables.WHITE, start_pos=(j * self.size, 0), end_pos=(j * self.size, variables.SIZE[1]), width=5)
-
-        # Check if the mouse position is within the grid boundaries
-        if self.position_in_table(mouse_pos):
-            x, y = self.position_in_table(mouse_pos)
-            # Create a new piece and add it to the grid
-            piece = self.get_piece(piece)
-            self.store_pieces.append({"piece": piece, "position": (x, y)})
-
-        for dict in self.store_pieces:
-            piece_value = dict.get("piece")
-            surface.blit(piece_value.get_circular_surface(60), dict.get("position"))
-
+        
+        now = Piece(piece)        
+        self.store_pieces.append({"piece": now, "position": now.position_in_table(mouse_pos)})
+        
+        for dictionary in self.store_pieces:
+            surface.blit(dictionary.get("piece").get_circular_surface(), dictionary.get("position"))      
+                
         pygame.display.update()
-        
-    def position_in_table(self, mouse_pos):
-        x, y =  mouse_pos 
-        position = [15, 103, 190, 277, 365, 451, 539, 627, 700]
-        
-        for i in range(len(position) - 1):
-            if x in range(position[i], position[i + 1]):
-                x = position[i]
-                
-        for j in range(len(position) - 1):
-            if y in range(position[j], position[j + 1]):
-                y = position[j]
-                
-        return (x, y)
-
-    def get_piece(self, piece):
-        if piece % 2 == 0:
-            piece = Piece(variables.WHITE)
-        else:
-            piece = Piece(variables.BLACK)
-            
-        return piece
-            
         
         
