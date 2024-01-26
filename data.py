778f041731f4a8404d930e8ecbf5a913cb23a49f
Maky188 Kali Linux <maky188pgt555@gmail.com>
2024-01-26 18:29:20 +0000

    Solved bug

--- data.py
+++ data.py
@@ -10,6 +10,7 @@ def get_pieces(number_pieces):
     list_of_pieces[0].position_in_table((277, 365))
     list_of_pieces[1].position_in_table((365, 365))
     list_of_pieces[2].position_in_table((365, 277))
+    list_of_pieces[3].position_in_table((277, 277))
             
     return list_of_pieces
 
